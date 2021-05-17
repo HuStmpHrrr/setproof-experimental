d3396aab9af66ad55f2575601196c58f67620a36
HuStmpHrrr <fdhzs2010@hotmail.com>
2021-05-17 12:41:39 -0400

    update AST

--- src/ast.ml
+++ src/ast.ml
@@ -1,7 +1,24 @@
 open Lib
 
+type case =
+  | IndCase of {
+      tm_ind_name : string;
+      tm_constr   : string location;
+      (* similar to telescope, in reverse order *)
+      tm_args     : pattern list;
+    }
+  | EqCase of pattern
+(* a pattern is nested, either a variable (matches all) or a pattern *)
+and pattern =
+  (* wildcard is None, otherwise it's Some *)
+  | PVar of (string option) location
+  (* nested pattern *)
+  | PCase of case
+
 type tm =
   | U of int * loc              (* universe, with nat level *)
+  (* a global reference to some definition *)
+  | Glob of string location
   (* variable with de Bruijn index. remember string name (None when wildcard) and location *)
   | Var of int * (string option) location
   (* we then look into type definitions *)
@@ -31,23 +48,12 @@ type tm =
   | Case of tm * (case * tm) list * loc
   | Refl of tm * loc            (* location for the refl header *)
 and ty = tm
-and case =
-  | IndCase of {
-      tm_ind_name : string;
-      tm_constr   : string location;
-      (* similar to telescope, in reverse order *)
-      tm_args     : pattern list;
-    }
-  | EqCase of (string option) location
-and pattern =
-  | PVar of (string option) location
-  | PCase of case
 
 (* a telescope is a reverse context, which is good for unification *)
 type telescope = ty list
 
 type qit_def = {
-    qit_def_loc : loc;          (* definition location *)
+    qit_name    : string location; (* definition name and location *)
     qit_index   : telescope;     (* a telescope for the indices *)
     qit_indexed : telescope;     (* a telescope for the argument *)
     qit_ret_lv  : int;           (* the level of universe in which the inductive definition reside *)
@@ -65,9 +71,11 @@ let rec telescope_to_pi ts rt : ty =
   | [] -> rt
   | t :: ts' -> Pi (t, telescope_to_pi ts' rt)
 
+(* location info for a term *)
 let rec tm_loc t : loc =
   match t with
   | U (_, l)          -> l
+  | Glob n            -> loc_erase n
   | Var (_, n)        -> loc_erase n
   | Pi (a, b)         -> loc_combine (tm_loc a) (tm_loc b)
   | Ind n             -> loc_erase n
