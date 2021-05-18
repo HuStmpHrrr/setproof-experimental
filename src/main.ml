dd9d362c8d95b66e0f27e7cc42843c07b38a729d
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-05-18 12:14:45 -0400

    Apply format

--- src/main.ml
+++ src/main.ml
@@ -2,9 +2,7 @@ open Base
 open Setproof
 
 let _ =
-  try
-    Parser.parse_file (Sys.get_argv ()).(1)
-  with
-  | Parser.ParserError msg ->
-     Stdio.Out_channel.output_string Stdio.stderr msg;
-     Caml.exit 0
+  try Parser.parse_file (Sys.get_argv ()).(1)
+  with Parser.ParserError msg ->
+    Stdio.Out_channel.output_string Stdio.stderr msg;
+    Caml.exit 0
