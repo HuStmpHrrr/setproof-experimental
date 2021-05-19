35a452a5c7b98b72785a4a03887c98bf1c0eb03f
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-05-19 02:25:26 -0400

    Use earley's parser

--- src/main.ml
+++ src/main.ml
@@ -1,12 +1,9 @@
 open Base
 open Setproof
 
+let _ = Earley_core.Earley.debug_lvl := 0
+
 let _ =
-  try
-    (Sys.get_argv ()).(1)
-    |> Parser.parse_file
-    |> Elaborate.elab_module_def
-  with
-  | Parser.ParserError msg ->
-      Stdio.Out_channel.output_string Stdio.stderr msg;
-      Caml.exit 0
+  (Sys.get_argv ()).(1)
+  |> Earley_core.Earley.handle_exception Parser.parse_file
+  |> Elaborate.elab_module_def
--- src/parser.ml
+++ src/parser.ml
@@ -1,55 +1,129 @@
 open Base
-module I = Grammar.MenhirInterpreter
-module S = MenhirLib.General
+open Earley_core
+module Ext = Ext_ast
 
-exception ParserError of string
+(* Location function. *)
+let locate str1 start_c str2 end_c =
+  let file = Input.filename str1 in
+  let start_l  = Input.line_num str1 in
+  let end_l  = Input.line_num str2 in
+  Lib.(RealLoc { file; start_l; start_c; end_l; end_c; data = () })
 
-let pp_position fmt (p : Caml.Lexing.position) =
-  Caml.Format.fprintf fmt "%s : line %d, column %d" p.pos_fname p.pos_lnum
-    (p.pos_cnum - p.pos_bol)
+#define LOCATE locate
 
-let state env : int =
-  match Lazy.force (I.stack env) with
-  | S.Nil -> 0 (* hacky initial state *)
-  | S.Cons (Element (s, _, _, _), _) -> I.number s
+let _ = Earley.keep_all_names := true
 
-let rec loop next_token lexbuf (checkpoint : Ext_ast.module_def I.checkpoint) =
-  match checkpoint with
-  | I.InputNeeded _env ->
-      let token = next_token () in
-      let checkpoint = I.offer checkpoint token in
-      loop next_token lexbuf checkpoint
-  | I.Shifting _
-  | I.AboutToReduce _ ->
-      let checkpoint = I.resume checkpoint in
-      loop next_token lexbuf checkpoint
-  | I.HandlingError env ->
-      raise
-        (ParserError
-           (Caml.Format.asprintf
-              Caml.(
-                ""
-                ^^ "************************************************************@\n"
-                ^^ "  @[<v2>!!! Syntax error in %d@,at %a@]@\n"
-                ^^ "************************************************************@\n")
-              (state env) pp_position
-              (fst (Sedlexing.lexing_positions lexbuf))))
-  | I.Accepted module_def -> module_def
-  | I.Rejected ->
-      (* Cannot happen as we stop at syntax error immediatly *)
-      assert false
+let ident_rest = "[_a-zA-Z0-9]"
 
-let parse_channel name chan =
-  let lexbuf = Sedlexing.Utf8.from_channel chan in
-  Sedlexing.set_filename lexbuf name;
-  let lexer = Lexer.gen_lexer lexbuf in
-  try
-    loop lexer lexbuf
-      (Grammar.Incremental.modDef (fst @@ Sedlexing.lexing_positions lexbuf))
-  with
-  | Lexer.InvalidToken (line, column, tok) ->
-      raise
-        (ParserError
-           (Caml.Format.asprintf "lexing error : %s at %d:%d%!" tok line column))
+module K =
+  Keywords.Make
+    (struct
+      let id_charset = Charset.from_string ident_rest
+      let reserved = []
+    end)
 
-let parse_file file = Stdio.In_channel.with_file ~f:(parse_channel file) file
+let key_Univ = K.create "Univ"
+let key_Refl = K.create "Refl"
+let key_lam = K.create "lam"
+let key_pi = K.create "pi"
+let key_match = K.create "match"
+let key_with = K.create "with"
+let key_quotient = K.create "quotient"
+let key_data = K.create "data"
+let key_where = K.create "where"
+
+let blank = Blanks.line_comments "--"
+
+let parser integer =
+  i:RE("0\\|[1-9][0-9]") -> Int.of_string i
+
+let parser lower_ident =
+  i:RE("\\(_\\|[a-z]\\)" ^ ident_rest ^ "*") -> K.check i; i
+
+let parser upper_ident =
+  i:RE("[A-Z]" ^ ident_rest ^ "*") -> K.check i; i
+
+let parser branch tm = p:pattern "->" t:tm ';' -> (p, t)
+and parser pattern = pattern0
+and parser pattern0 =
+  | cid:upper_ident args:pattern1* -> Ext.PatInd(Lib.loc_combine _loc_cid _loc_args, cid, args)
+  | _s:key_Refl p:pattern1         -> Ext.PatEq(Lib.loc_combine _loc__s _loc_p, p)
+  | pattern1
+and parser pattern1 =
+  | id:lower_ident    -> Ext.PatVar (_loc_id, id)
+  | '(' p:pattern ')' -> p
+
+let parser lam_tm (ty, tm) =
+  _s:key_lam id:lower_ident ':' tau:ty ',' t:tm -> Ext.TmLam(Lib.loc_combine _loc__s _loc_t, id, tau, t)
+
+let parser pi_tm (ty, ty') =
+  _s:key_pi '(' id:lower_ident ':' tau:ty ')' "->" tau':ty' -> Ext.TmPi(Lib.loc_combine _loc__s _loc_tau', id, tau, tau')
+
+let parser arrow_tm (ty, ty') =
+  tau:ty "->" tau':ty' -> Ext.TmPi(Lib.loc_combine _loc_tau _loc_tau', "_", tau, tau')
+
+let parser match_tm (scr, br_tm) =
+  _s:key_match scr:scr key_with cbrs:(branch br_tm)* opt_qbrs:{key_quotient qbrs:(branch br_tm)* -> qbrs}? ->
+    (match opt_qbrs with
+     | Some qbrs -> Ext.TmMatch(Lib.loc_combine _loc__s _loc_opt_qbrs, scr, cbrs, qbrs)
+     | None      -> Ext.TmMatch(Lib.loc_combine _loc__s _loc_cbrs, scr, cbrs, []))
+
+let parser eq_tm tm =
+  lhs:tm '=' rhs:tm -> Ext.TmEq(Lib.loc_combine _loc_lhs _loc_rhs, lhs, rhs)
+
+let parser app_tm (tm, tm') =
+  f:tm a:tm' -> Ext.TmApp(Lib.loc_combine _loc_f _loc_a, f, a)
+
+let parser refl_tm tm =
+  _s:key_Refl t:tm -> Ext.TmRefl(Lib.loc_combine _loc__s _loc_t, t)
+
+let parser univ_tm =
+  _s:key_Univ i:integer -> Ext.TmUniv(Lib.loc_combine _loc__s _loc_i, i)
+
+let parser cstr_tm =
+  cid:upper_ident -> Ext.TmConstr(_loc_cid, cid)
+
+let parser var_tm =
+  id:lower_ident -> Ext.TmVar(_loc_id, id)
+
+let parser tm0 =
+  | (lam_tm (tm0, tm0))
+  | (pi_tm (tm0, tm0))
+  | (match_tm (tm0, tm0))
+  | (arrow_tm (tm1, tm0))
+  | tm1
+and parser tm1 =
+  | (eq_tm tm2)
+  | tm2
+and parser tm2 =
+  | (app_tm (tm2, tm3))
+  | (refl_tm tm3)
+  | univ_tm
+  | tm3
+and parser tm3 =
+  | cstr_tm
+  | var_tm
+  | tm4
+and parser tm4 =
+  | '(' t:tm0 ')' -> t
+and parser tm = tm0
+
+let parser fun_def =
+  i:lower_ident ':' tau:tm ":=" t:tm _e:';' -> Ext.FunDef(Lib.loc_combine _loc_i _loc__e, i, tau, t)
+
+let parser quot_ind_entry =
+  i:upper_ident ':' tau:tm _e:';' -> Ext.QuotIndEntryDef(Lib.loc_combine _loc_i _loc__e, i, tau)
+
+let parser quot_ind =
+  _s:key_data i:lower_ident idxs:{'(' i:lower_ident ':' tau:tm ')' -> (i, tau)}* ':' kappa:tm key_where
+    cstrs:quot_ind_entry*
+    quots:{key_quotient quots:quot_ind_entry* -> quots}?[[]]
+  _e:';' -> Ext.QuotIndDef(Lib.loc_combine _loc__s _loc__e, i, idxs, kappa, cstrs, quots)
+
+let parser top_stmt =
+  | f:fun_def  -> Ext.TopFunDef f
+  | q:quot_ind -> Ext.TopQuotInd q
+
+let parser mod_def = ts:top_stmt* EOF -> Ext.ModDef ts
+
+let parse_file = Earley.parse_file mod_def blank
